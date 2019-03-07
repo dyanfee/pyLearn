8430c6f6243015707871175e6bfecf099017893f
dyanfee <dyanfee@gmail.com>
2019-03-07 22:49:38 +0800

    views

--- Django/blogproject/blog/views.py
+++ Django/blogproject/blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
-def index(requset):
-    return render(request,'blog/index.html')
+
+def index(request):
+    post_list = Post.objects.all().order_by('-create_time')
+    return render(request, 'index.html', context={
+        "post_list": post_list
+    })
