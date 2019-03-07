5f6543ffc1e125ff409b5db41f800cc800857625
dyanfee <dyanfee@gmail.com>
2019-03-07 08:00:52 +0800

    blogpro

--- Django/blogproject/blog/models.py
+++ Django/blogproject/blog/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Category(models.Model):
+    name = models.CharField(max_length=100)
     """
     Django 要求模型必须继承 models.Model 类。
     Category 只需要一个简单的分类名 name 就可以了。
@@ -12,49 +13,42 @@ class Category(models.Model):
     Django 内置的全部类型可查看文档：
     https://docs.djangoproject.com/en/1.10/ref/models/fields/#field-types
     """
-    name = models.CharField(max_length=100)
 
 
 class Tag(models.Model):
-    """
-    标签 Tag 也比较简单，和 Category 一样。
-    再次强调一定要继承 models.Model 类！
-    """
     name = models.CharField(max_length=100)
 
 
 class Post(models.Model):
-    """
-    文章的数据库表稍微复杂一点，主要是涉及的字段更多。
-    """
-
-    # 文章标题
     title = models.CharField(max_length=70)
-
-    # 文章正文，我们使用了 TextField。
-    # 存储比较短的字符串可以使用 CharField，但对于文章的正文来说可能会是一大段文本，因此使用 TextField 来存储大段文本。
     body = models.TextField()
 
-    # 这两个列分别表示文章的创建时间和最后一次修改时间，存储时间的字段用 DateTimeField 类型。
-    created_time = models.DateTimeField()
+    create_time = models.DateTimeField()
     modified_time = models.DateTimeField()
 
-    # 文章摘要，可以没有文章摘要，但默认情况下 CharField 要求我们必须存入数据，否则就会报错。
-    # 指定 CharField 的 blank=True 参数值后就可以允许空值了。
     excerpt = models.CharField(max_length=200, blank=True)
 
-    # 这是分类与标签，分类与标签的模型我们已经定义在上面。
-    # 我们在这里把文章对应的数据库表和分类、标签对应的数据库表关联了起来，但是关联形式稍微有点不同。
-    # 我们规定一篇文章只能对应一个分类，但是一个分类下可以有多篇文章，所以我们使用的是 ForeignKey，即一对多的关联关系。
-    # 而对于标签来说，一篇文章可以有多个标签，同一个标签下也可能有多篇文章，所以我们使用 ManyToManyField，表明这是多对多的关联关系。
-    # 同时我们规定文章可以没有标签，因此为标签 tags 指定了 blank=True。
-    # 如果你对 ForeignKey、ManyToManyField 不了解，请看教程中的解释，亦可参考官方文档：
-    # https://docs.djangoproject.com/en/1.10/topics/db/models/#relationships
-    category = models.ForeignKey(Category)
+    #  ForeignKey、ManyToManyField
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, blank=True)
 
-    # 文章作者，这里 User 是从 django.contrib.auth.models 导入的。
-    # django.contrib.auth 是 Django 内置的应用，专门用于处理网站用户的注册、登录等流程，User 是 Django 为我们已经写好的用户模型。
-    # 这里我们通过 ForeignKey 把文章和 User 关联了起来。
-    # 因为我们规定一篇文章只能有一个作者，而一个作者可能会写多篇文章，因此这是一对多的关联关系，和 Category 类似。
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    '''
+        在django2.0后，定义外键和一对一关系的时候需要加on_delete选项，此参数为了避免两个表里的数据不一致问题，不然会报错：
+        TypeError: __init__() missing 1 required positional argument: 'on_delete'
+        举例说明：
+        user=models.OneToOneField(User)
+        owner=models.ForeignKey(UserProfile)
+        需要改成：
+        user=models.OneToOneField(User,on_delete=models.CASCADE) --在老版本这个参数（models.CASCADE）是默认值
+        owner=models.ForeignKey(UserProfile,on_delete=models.CASCADE) --在老版本这个参数（models.CASCADE）是默认值
+        参数说明：
+        on_delete有CASCADE、PROTECT、SET_NULL、SET_DEFAULT、SET()五个可选择的值
+        CASCADE：此值设置，是级联删除。
+        PROTECT：此值设置，是会报完整性错误。
+        SET_NULL：此值设置，会把外键设置为null，前提是允许为null。
+        SET_DEFAULT：此值设置，会把设置为外键的默认值。
+        SET()：此值设置，会调用外面的值，可以是一个函数。
+        一般情况下使用CASCADE就可以了。
+    '''
