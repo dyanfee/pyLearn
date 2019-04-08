c51a4d184f823345ae594d6bd0e68ebbbcb1fb24
dyanfee <dyanfee@gmail.com>
2019-04-08 17:18:23 +0800

    thread

---
+++ Thread/test111.py
@@ -0,0 +1,12 @@
+import math
+
+x = "\n"
+# print("dasd", x, 2, x, 3, x, 4, x, 34565, end="\n")
+
+
+
+print("dasd", 2,  3, 4, 34565, sep="\n")
+
+
+
+
--- test_file/tt.py
+++ test_file/tt.py
@@ -1,3 +1,22 @@
-lis = [1,3,4,2,5,8,7]
-lis.sort(key=lambda x:-x)
-print(lis)
+from multiprocessing import Pool
+import os
+import time
+import random
+
+
+def task(name):
+    print("运行任务：%s %s" % (name, os.getpid()))
+    start = time.time()
+    time.sleep(random.random()*3)
+    end = time.time()
+    print("%s任务执行了%s" % (name, end-start))
+
+
+if __name__ == "__main__":
+    print("父进程%s " % os.getpid())
+
+    p = Pool(50)
+    for i in range(6):
+        p.apply_async(task, args=(i,))
+    p.close()
+    p.join()
